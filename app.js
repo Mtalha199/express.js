1c21d71047d7faae422e2849c083f54f2664cb8a
Mtalha199 <talhatarar19@gmail.com>
2023-04-04 15:21:17 +0500

    route

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("this work on express js ")
+const express=require('express')
+const app=express();
+
+app.get('/',(req,res)=>{
+res.send("this is home page of express")
+})
+
+app.get('/about',(req,res)=>{
+    res.send("this is about page of express")
+    })
+
+app.listen(5000,()=>{
+    console.log("listning on port 5000")
+})
